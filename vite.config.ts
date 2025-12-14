5f6c908e12b548a6242cd2bc23e64882ffaa2a86
Nick Feng <165098673+nfe7@users.noreply.github.com>
2025-12-13 23:32:51 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()]
+      plugins: [react()],
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
